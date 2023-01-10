0dbf28b91f07c174d8bf23446f494e73c93f2adf
Gerome Antido <gantido49@mmsu.edu.ph>
2023-01-10 15:46:57 +0800

    Added css

---
+++ login.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head css>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+    <body>
+        <div class="hug">
+            <div class="left">
+                <img class="imgf" src="img/mmsuadmin.jpg" alt="">
+            </div>
+            <div class="right">
+                <form class="form" action="">
+                    <div class="emptydiv"></div>
+                    <div class="headframe">
+                        <img class="img" src="img/mmsulogo.png" alt="">
+                        <h1 class="titletext">Event Attendance Portal</h1>
+                    </div>
+                    <div class="formframe">
+                        <div>
+                            <label class="label" for="username">Username</label>
+                            <input class="inputbox" type="text" name="username">
+                        </div>
+                        <div>
+                            <label class="label" for="password">Password</label>
+                            <input class="inputbox" type="password" name="password">  
+                        </div>
+                    </div>
+                    <button class="loginbtn"><p class="text">Login<p></button>`
+                </form>
+            </div>
+        </div>
+        
+    </body>
+</html>
