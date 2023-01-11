235e992dae385f749ac0e3012c506a9c6deb8fa3
UserNode <52162412+UnknownProxyGitHub@users.noreply.github.com>
2023-01-11 21:06:34 +0800

    Dashboard ( Draft 2 )

--- dashboard.php
+++ dashboard.php
@@ -5,12 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
-    <link rel="stylesheet" href="../css/dashboard.css">
+    <link rel="stylesheet" href="css/dashboard.css">
     <script src="https://kit.fontawesome.com/b8e4159bd9.js" crossorigin="anonymous"></script>
 </head>
     <body>
+
         <?php  
-        include("php/auth.php")
+        // include("php/auth.php")
         ?>
 
         <!-- nav-dashboard-container start --->
@@ -52,8 +53,8 @@
                         </li>
                         <li>
                             <i class="fa-solid fa-arrow-right-from-bracket"></i>
-                            <form action="" method="POST">
-                                <input name="logout" type="submit"><span>Log Out</span></input>
+                            <form action="#" method="POST">
+                                <input name="logout" type="submit" value="Log Out">
                             </form>
                         </li>
                     </ul>
