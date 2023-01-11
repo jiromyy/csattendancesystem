c403f0f9dacdc22335ee5f9b1cdfb2e868bb7523
UserNode <52162412+UnknownProxyGitHub@users.noreply.github.com>
2023-01-11 20:33:58 +0800

    dashboard

--- dashboard.php
+++ dashboard.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
-    <link rel="stylesheet" href="css/dashboard.css">
+    <link rel="stylesheet" href="../css/dashboard.css">
     <script src="https://kit.fontawesome.com/b8e4159bd9.js" crossorigin="anonymous"></script>
 </head>
     <body>
