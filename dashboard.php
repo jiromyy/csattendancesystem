b4bd21a92069ed63e9046f10cab021736a8c9553
UserNode <52162412+UnknownProxyGitHub@users.noreply.github.com>
2023-01-18 13:44:41 +0800

    dashboard (Draft 4)

--- dashboard.php
+++ dashboard.php
@@ -55,7 +55,7 @@
                         </a>
                     </li>
                     <li <?php // if ($user != "org_manager" && $user != "super_admin") echo 'style=display:none'?>>
-                        <a href="manage-events">
+                        <a href="manage-events.php">
                             <i class="fa-regular fa-note-sticky"></i>
                             <span>Events</span>
                         </a>
