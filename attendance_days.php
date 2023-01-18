93301b7c601feb4a6397060cdb9d3dda5538f40a
Emzkie The Great <63085286+hwangxiaoman@users.noreply.github.com>
2023-01-19 00:41:21 +0800

    Days page Creation

---
+++ attendance_days.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en">
+<head css>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Attendance Report | Days</title>
+    <link rel="stylesheet" href="css/attendance_days.css">
+    <script src="https://kit.fontawesome.com/b8e4159bd9.js" crossorigin="anonymous"></script>
+</head>
+    <body>
+                <?php
+                    include("navbar.php");
+                ?>
+                <div class="content-area">
+                    <div class="section-1">
+                        <a href="attendance_events.php"><span>Events &nbsp;</span></a>
+                        <a href="attendance_days.php"><span>&gt; &nbsp;Days</span></a>
+                    </div>
+                </div>
+            </main>
+        </div>
+
+        <script src="script/attendance_events.js"></script>
+    </body>
+</html>
